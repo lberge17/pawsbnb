ca2e1937cd35ea299b80e231e4921d51ba575764
Laura Berge <lauraeberge@gmail.com>
2020-01-22 10:48:22 -0500

    fixes import file name

--- pawsbnb-client/src/App.js
+++ pawsbnb-client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Breeds from './component/Breeds'
+import Breeds from './components/Breeds'
 
 function App() {
   return (

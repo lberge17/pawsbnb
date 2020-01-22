9f1e2a760b01f01bbc2c46c43c606f98f98384fc
Laura Berge <lauraeberge@gmail.com>
2020-01-22 18:48:51 -0500

    adds react router

--- pawsbnb-client/src/App.js
+++ pawsbnb-client/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/app/Navbar'
-import Home from './components/app/Home'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
-        <Home />
       </header>
     </div>
   );
--- pawsbnb-client/src/index.js
+++ pawsbnb-client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/app/Home'
+import About from './components/app/About'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <Router>
+        <Route exact path='/' component={Home}></Route>
+        <Route exact path='/about' component={About}></Route>
+    </Router>
+    ), document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

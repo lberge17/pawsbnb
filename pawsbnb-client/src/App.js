41ba269cbb1da4b20a29ce0c9bcca248de1b5dce
Laura Berge <lauraeberge@gmail.com>
2020-01-23 17:52:32 -0500

    adds a logout function, user authentication now fully functioning, adds a dashboard component for signed in users

--- pawsbnb-client/src/App.js
+++ pawsbnb-client/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/home/Home'
+import Dashboard from './components/home/Dashboard'
 import About from './components/about/About'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
+import axios from 'axios'
 
 export default class App extends Component {
   constructor(props) {
@@ -26,6 +28,41 @@ export default class App extends Component {
       loggedIn: true,
       user: data.user
     })
+  }
+
+  handleLogout = () => {
+    axios.delete("http://localhost:3000/logout", { withCredentials: true })
+      .then(resp => {
+        if(resp.data.logged_out) {
+          this.setState({
+            loggedIn: false
+          })
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
+  checkLoginStatus = () => {
+    axios.get("http://localhost:3000/logged_in", { withCredentials: true })
+      .then(resp => {
+        console.log(resp)
+        if (resp.data.logged_in) {
+          this.setState({
+            loggedIn: resp.data.logged_in,
+            user: resp.data.user
+          })
+        } else {
+          this.setState({
+            loggedIn: false,
+            user: {}
+          })
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.checkLoginStatus()
   }
 
   render () {
@@ -56,7 +93,14 @@ export default class App extends Component {
               <About { ...props } loggedIn={this.state.loggedIn} />
             )}
           />
+          <Route
+            exact path='/dashboard'
+            render={props => (
+              <Dashboard { ...props } loggedIn={this.state.loggedIn} />
+            )}
+          />
         </Router>
+        {this.state.loggedIn ? <button onClick={this.handleLogout}>Logout</button> : null}
       </div>
     );
   }
--- pawsbnb-client/src/components/home/Home.js
+++ pawsbnb-client/src/components/home/Home.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import Logo from './images/pawsbnb-logo.png'
-import Navbar from './Navbar'
 import './css/home.css';
 
 export default class Home extends Component {
@@ -9,28 +8,26 @@ export default class Home extends Component {
     }
 
     render() {
+        if (this.props.loggedIn) {
+            this.props.history.push('./dashboard')
+        }
         return (
-            <div className="App">
-                <header className="App-header">
-                    <Navbar/>
-                    <div className="home-wrapper">
-                        <div className="home">
-                            <a className="panel img1" href="#">
-                                <p className="home-pg-link">Provider Login</p>
-                            </a>
-                            <a className="panel img2" href="#">
-                                <p className="home-pg-link">{this.props.loggedIn ? 'you are logged in' : 'please log in'}</p>
-                            </a>
-                            <a className="panel img3" href="#">
-                                <p className="home-pg-link">Pet Breed Info</p>
-                            </a>
-                            <a className="panel img4" href="/about">
-                                <p className="home-pg-link">About PawsBnB</p>
-                            </a>
-                        </div>
-                        <img className="centered-logo" src={Logo}/>
-                    </div>
-                </header>
+            <div className="home-wrapper">
+                <div className="home">
+                    <a className="panel img1" href="#">
+                        <p className="home-pg-link">Provider Login</p>
+                    </a>
+                    <a className="panel img2" href="#">
+                        <p className="home-pg-link">{this.props.loggedIn ? 'you are logged in' : 'please log in'}</p>
+                    </a>
+                    <a className="panel img3" href="#">
+                        <p className="home-pg-link">Pet Breed Info</p>
+                    </a>
+                    <a className="panel img4" href="/about">
+                        <p className="home-pg-link">About PawsBnB</p>
+                    </a>
+                </div>
+                <img className="centered-logo" src={Logo}/>
             </div>
         )
     }

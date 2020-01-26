91a1b0a928f08d9b9dd783136fbc538e0e0df771
Laura Berge <lauraeberge@gmail.com>
2020-01-26 09:58:19 -0500

    adds more functionality to calendar and pulls info from events when they are clicked or dragged

--- pawsbnb-client/src/components/home/Calendar.js
+++ pawsbnb-client/src/components/home/Calendar.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+// import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
 export default class Calendar extends React.Component {
 
+    handleDateClick = (arg) => {
+        console.log(arg.dateStr)
+    }
+
+    handleEventDrop = (info) => {
+        console.log(info.event.start)
+        let start = toString(info.event.start).split(' ').slice(1,4)
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        const ind = months.findIndex(month => month === start[0])
+        // start[0] = ind + 1
+        console.log(info.event.start.toISOString())
+    }
+
+    handleEventClick= ({event}) => {
+        console.log(event)
+    }
+
     render() {
       return (
-        <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+        <FullCalendar 
+            defaultView="dayGridMonth" 
+            plugins={[dayGridPlugin, interactionPlugin]}
+            dateClick={this.handleDateClick}
+            editable={true}
+            eventDrop={this.handleEventDrop}
+            eventClick={this.handleEventClick}
+            events={[{title: "Event", start: '2020-01-26', end: '2020-01-28'}, {title: 'event 2', start: '2020-01-15T24:00:00.000Z', id: "1", extendedProps: {details: "boarding", pet: "Millie"}}]}
+        />
       )
     }
   
--- pawsbnb-client/src/components/home/Dashboard.js
+++ pawsbnb-client/src/components/home/Dashboard.js
@@ -5,13 +5,20 @@ import './css/dashboard.css'
 
 export default class Dashboard extends Component {
     state = {
-        form: false
+        form: false,
+        toggleHam: false
     }
 
     handleFormLoad = () => {
         console.log('handling form load')
         this.setState({
             form: true
+        })
+    }
+
+    toggle = () => {
+        this.setState(prevState => {
+            return {toggleHam: !prevState.toggleHam}
         })
     }
 
@@ -24,11 +31,21 @@ export default class Dashboard extends Component {
 
         return (
             <div>
-                <div>{this.state.form ? 
-                    <BusinessForm user={this.props.user} business={this.props.business} location={this.props.location}/> :
-                    <button onClick={this.handleFormLoad}>{this.props.business.title ? 'Edit Your Business': 'Add Your Business'}</button>
-                }</div>
-                <div class="calendar-container">
+                <button className="hamIcon" onClick={this.toggle}><h1>&#9776;</h1></button>
+                <div className="side-container">
+                    <div id="mySidebar" className={this.state.toggleHam ? "sidebar toggle" : "sidebar"}>
+                        <a href="#" className="close" onClick={this.toggle}>&times;</a>
+                        <a href="#" onClick={this.handleFormLoad}>Add/Update Business</a>
+                        <a href="#">Clients</a>
+                        <a href="#">Add Appointments</a>
+                    </div>
+                </div>
+
+                <h1>{this.props.business.title ? this.props.business.title : 'Please click the menu icon to add your Business'}</h1>
+                {this.state.form ? 
+                    <BusinessForm user={this.props.user} business={this.props.business} location={this.props.location}/> : null
+                }
+                <div className="calendar-container">
                     <h1>Your Calendar</h1>
                     <Calendar/>
                 </div>

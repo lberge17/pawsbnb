b66e252849074a9a2000cd06f6ea9ab54f0f0fbc
Laura Berge <lauraeberge@gmail.com>
2020-01-26 12:01:56 -0500

    renders a table of client data

--- pawsbnb-client/src/components/clients/Client.js
+++ pawsbnb-client/src/components/clients/Client.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 
 export default class Client extends Component {
     render(){
-        return <div>{this.props.client.name}</div>
+        return (
+            <tr>
+                <td>{this.props.client.name}</td>
+                <td>{this.props.client.phone}</td>
+                <td>{this.props.client.email}</td>
+                <td>{this.props.client.address}</td>
+                <td>{this.props.client.emergency_contact}</td>
+            </tr>
+        )
     }
 }
--- pawsbnb-client/src/components/clients/Clients.js
+++ pawsbnb-client/src/components/clients/Clients.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Client from './Client'
 import Axios from 'axios'
+import './css/clients.css'
 
 export default class Clients extends Component {
     state = {
@@ -17,6 +18,19 @@ export default class Clients extends Component {
             })
     }
     render(){
-        return <div>{this.state.clients.map(client => <Client client={client}/>)}</div>
+        console.log(this.state.clients)
+        return (
+            <table style={{width: '100%'}} className="client-tbl">
+                <caption style={{textAlign: 'center'},{fontSize: '3em'}}>Clients List</caption>
+                <tr>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Emergency Contact</th>
+                </tr>
+                {this.state.clients[0] ? this.state.clients.map(client => <Client client={client}/>) : null}
+            </table>
+        )
     }
 }

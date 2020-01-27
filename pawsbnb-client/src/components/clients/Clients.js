f4ccc9065db4cc0ca176a616b65f18a01631d048
Laura Berge <lauraeberge@gmail.com>
2020-01-26 20:03:57 -0500

    adds loading message if the fetch is still running from the store

--- pawsbnb-client/src/components/clients/Clients.js
+++ pawsbnb-client/src/components/clients/Clients.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 import { ClientRow } from './ClientRow'
 import Client from './Client'
 import ClientForm from './ClientForm'
-import Axios from 'axios'
 import './css/clients.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { connect } from 'react-redux';
 import { addClient, removeClient, fetchClients } from '../../actions/clientsActions'
 
@@ -46,6 +45,7 @@ class Clients extends Component {
         return (
             <div>
                 <h1 style={{textAlign: 'center'},{fontSize: '3em'}}>Clients List</h1>
+                {this.props.clientsInfo.requesting ? <div>loading...</div> : null}
                 {this.state.clientForm ? <ClientForm toggleClientForm={this.toggleClientForm}/> : <button onClick={this.toggleClientForm}>Add new Client</button>}
                 {this.state.client ? <Client client={this.state.client} removeClientPopup={this.removeClientPopup}/> : null}
                 <table style={{width: '100%'}} className="client-tbl">

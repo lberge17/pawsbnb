c93b2a0b2258c2759f2789bb3833a250c6fcc031
Laura Berge <lauraeberge@gmail.com>
2020-01-27 08:33:29 -0500

    set up backend for adding appointments

--- pawsbnb-backend/app/controllers/appointments_controller.rb
+++ pawsbnb-backend/app/controllers/appointments_controller.rb
@@ -1,2 +1,39 @@
 class AppointmentsController < ApplicationController
+    def index
+        if @current_user && @current_user.business
+            render json: @current_user.business.appointments
+        else
+            render json: {
+                status: 401
+            }
+        end
+    end
+
+    def create
+        if @current_user && @current_user.business && @current_user.business.clients.find_by(id: params['appointment']['client_id'])
+            appointment = Appointment.new(appointment_params)
+            appointment.business = @current_user.business
+            if appointment.save
+                render json: {
+                    status: :created,
+                    appointment: appointment
+                }
+            else
+                render json: {
+                    status: 500,
+                    error: appointment.errors.full_messages
+                }
+            end
+        else
+            render json: {
+                status: 401
+            }
+        end
+    end
+
+    private
+
+    def appointment_params
+        params.appointment.require(:title, :start, :end, :pets, :services, :medications, :details, :client_id)
+    end
 end
